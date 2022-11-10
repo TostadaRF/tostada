bc1fe6196ce098c36bdee7a87070f6f6ccc8812c
Alberto <a.riverofabelo@gmail.com>
2022-11-10 12:52:47 +0000

    Subo archivo

---
+++ cambiarVocales.py
@@ -0,0 +1,43 @@
+def subCadena(texto,inicio,fin):
+    resultado = ''
+    if inicio <= fin and inicio < len(texto) and fin < len(texto):
+        while inicio <= fin :
+            resultado += texto[inicio]
+            inicio += 1
+    return resultado
+
+def posCadena(texto,subtexto):
+    i = 0
+    while i+len(subtexto) <= len(texto) :
+        if subCadena(texto,i,i+len(subtexto)-1) == subtexto:
+            return i
+        else:
+            i+=1
+    return -1
+
+def estaEnArray(array,valor):
+    i = 0
+    while i<= len(array)-1 :
+        if array[i] == valor :
+            return True
+        else :
+            i += 1
+    return False
+
+print("Este programa lee una cadena de texto y devuelve la misma cadena intercambiando cada vocal por la siguiente\n")
+texto_prueba = input("Introduce el texto\n")
+
+vocales = "aeiouaáéíóúáAEIOUAÁÉÍÓÚÁ"
+Auxiliar = ""
+i = 0
+
+while i < len(texto_prueba):
+    if estaEnArray(vocales,texto_prueba[i]) == True:
+        Auxiliar += vocales[posCadena(vocales,texto_prueba[i])+1]
+        i+=1
+    else:
+        Auxiliar += texto_prueba[i]
+        i+=1
+
+
+print(Auxiliar)
